be020a47408018cad0514f1700173d07f17c88af
Trev <trevor.pawlewicz@gmail.com>
2015-07-08 16:54:46 -0700

    modal, audio, chip animation

---
+++ js/modal-init.js
@@ -0,0 +1,11 @@
+(function(){
+  var $content = $('#share-options').detach();   // Remove modal from page
+
+  modal.open({content: $content, width:800, height:720});
+
+  /*
+  $('#share').on('click', function() {           // Click handler to open modal
+    modal.open({content: $content, width:340, height:300});
+  });
+  */
+}());
